3c9961d983134e1a802e9c2a9f096f9f5bd0fe70
Jack Doy <jackdoy99@icloud.com>
2022-09-26 16:21:38 +0100

    Updated animals

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,10 @@
+import { Component } from "react";
+import "./Footer.css";
+
+class Footer extends Component {
+  render() {
+    return <div className="footer">&copy; Jack Doy</div>;
+  }
+}
+
+export default Footer;
